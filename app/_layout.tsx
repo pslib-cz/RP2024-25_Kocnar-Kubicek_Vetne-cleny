208bcb3e972db6f21dfefa6d54fc47f439d713b2
Tomáš <tomas.kubicek.022@pslib.cz>
2025-04-02 19:16:08 +0200

    ja ti to pushnu

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { Text } from 'react-native';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { ThemedView } from '@/components/ThemedView';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -15,6 +17,8 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    Outfit: require('../assets/fonts/Outfit-VariableFont_wght.ttf'),
+    //PT: require('../assets/fonts/PTRegular-Regular.woff2'),
   });
 
   useEffect(() => {
@@ -29,11 +33,13 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+        {/* <ThemedView style={{ fontFamily: 'Outfit' }}> */}
+            <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+                <Stack.Screen name="+not-found" />
+            </Stack>
+            <StatusBar style="auto" />
+        {/* </ThemedView> */}
     </ThemeProvider>
   );
 }
