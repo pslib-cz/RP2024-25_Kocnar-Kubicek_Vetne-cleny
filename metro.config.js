ed90afcf0e03c68d551680e7439f7959b67a4881
tkdeeev <tomas.kubicek.022@pslib.cz>
2026-06-06 11:22:56 +0200

    fix package errors

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require('@expo/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
 
 const configs = getDefaultConfig(__dirname);
 configs.resolver.assetExts.push('csv') // this is necessary (I think)
